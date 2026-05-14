9b355c840d830732b97ad6cd0f468d6036174bd4
shey <shey@shey.ca>
2026-05-14 18:55:18 -0400

    upload two images, adjust for brightness

--- capture-and-upload.sh
+++ capture-and-upload.sh
@@ -4,28 +4,30 @@ set -euo pipefail
 readonly VIDEO_DEVICE="/dev/video0"
 readonly VIDEO_SIZE="2048x1536"
 
-readonly BUCKET=""
-readonly PREFIX="v1"
+readonly BUCKET="${PLANT_CAM_BUCKET:?PLANT_CAM_BUCKET is required}"
+readonly PREFIX="plant-cam"
 
 configure_camera() {
   v4l2-ctl -d "${VIDEO_DEVICE}" \
     --set-ctrl=power_line_frequency=2 \
-    --set-ctrl=white_balance_automatic=0 \
-    --set-ctrl=white_balance_temperature=4600 \
-    --set-ctrl=auto_exposure=1 \
-    --set-ctrl=exposure_time_absolute=625 \
-    --set-ctrl=gain=0 \
-    --set-ctrl=sharpness=4
+    --set-ctrl=white_balance_automatic=1 \
+    --set-ctrl=brightness=115 \
+    --set-ctrl=contrast=50 \
+    --set-ctrl=saturation=80
 }
 
-build_s3_uri() {
+build_timestamped_s3_uri() {
   local timestamp
   local object_key
 
   timestamp="$(date +%Y%m%d-%H%M%S)"
-  object_key="${PREFIX}/image-${timestamp}.jpg"
+  object_key="${PREFIX}/plant-${timestamp}.jpg"
 
   echo "s3://${BUCKET}/${object_key}"
+}
+
+build_latest_s3_uri() {
+  echo "s3://${BUCKET}/latest.jpg"
 }
 
 capture_image() {
@@ -36,8 +38,10 @@ capture_image() {
     -video_size "${VIDEO_SIZE}" \
     -i "${VIDEO_DEVICE}" \
     -frames:v 1 \
+    -vf "unsharp=5:5:1.0:5:5:0.0, eq=gamma=0.8:saturation=1.1" \
     -f image2pipe \
     -vcodec mjpeg \
+    -q:v 2 \
     -
 }
 
@@ -51,12 +55,17 @@ upload_image() {
 }
 
 main() {
-  local s3_uri
+  local timestamped_s3_uri
+  local latest_s3_uri
 
   configure_camera
 
-  s3_uri="$(build_s3_uri)"
-  capture_image | upload_image "${s3_uri}"
+  timestamped_s3_uri="$(build_timestamped_s3_uri)"
+  latest_s3_uri="$(build_latest_s3_uri)"
+
+  capture_image \
+    | tee >(upload_image "${latest_s3_uri}") \
+    | upload_image "${timestamped_s3_uri}"
 }
 
 main "$@"
